dcf29d0ffe1fc9b93f641835d4fbc62592641384
BeToast <note2bp@protonmail.com>
2023-06-24 04:37:27 +0100

    that was a big bang

---
+++ src/routes/Home.tsx
@@ -0,0 +1,12 @@
+// import {  } from "../staticExports";
+import '/src/css/home.css';
+
+function Home() {
+  return (
+    <main class="main">
+      <h1>Rely on the Lord alone, else you can do nothing</h1>
+    </main>
+  );
+}
+
+export default Home;
---
+++ src/routes/Page2.tsx
@@ -0,0 +1,12 @@
+// import {  } from "../staticExports";
+import '/src/css/page2.css';
+
+function Page2() {
+  return (
+    <main class="main">
+      <h1>If this is not for the glory of God. QUIT!</h1>
+    </main>
+  );
+}
+
+export default Page2;
